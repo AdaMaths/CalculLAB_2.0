6e94fd82c04173fc4fa2adb47d9034b20f121fd6
AdaMaths <adama.gueye.3304@gmail.com>
2025-06-20 13:30:00 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 import streamlit as st
-from ui.page_accueil import main_page  # Adapte ce import selon ton projet
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui.page_accueil import MainWindow
 
 def main():
-    main_page()  # Affiche la page principale dans Streamlit
-
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
 if __name__ == "__main__":
     main()
